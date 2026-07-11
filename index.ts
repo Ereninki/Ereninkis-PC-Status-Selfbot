6e062dd6a7cf3e4aacf1206173554ff8ae4516b6
Ereninki <128488683+Ereninki@users.noreply.github.com>
2026-07-11 11:38:36 +0300

    fixed a problem that wont change your status and changed the process of editing your status

--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
 import { App } from "@slack/bolt";
 import { WebClient } from "@slack/web-api";
 import si from "systeminformation";
+import { collapseTextChangeRangesAcrossMultipleVersions, isReadonlyKeywordOrPlusOrMinusToken } from "typescript";
 
 const client = new WebClient(process.env.SLACK_USER_TOKEN);
-var status = 99;
+var status = -1;
 var laststatus = 0;
+let isramok: boolean;
+let iscpuok: boolean;
+let isrammid: boolean;
+let iscpumid: boolean;
+let iscpuisdying: boolean;
+let isramisdying: boolean;
+
 
 async function getsysteminfo() {
     var cpuload = await si.currentLoad();
@@ -23,10 +31,48 @@ const app = new App ({
     socketMode: true,
 });
 
-async function updatestatus() {
+async function getstatusvalue() {
     var { ramusage, cpuusage } = await getsysteminfo();
 
-    if (ramusage >= 99 && cpuusage >= 99) {
+    if (ramusage >= 75) {
+        isrammid = true;
+        isramok = false;
+        isramisdying = false;
+    } else if (ramusage < 75) {
+        isramisdying = false;
+        isrammid = false;
+        isramok= true;
+    } if (ramusage <= 99){
+        isramok = false;
+        isrammid = false;
+        isramisdying = true;
+    }
+
+    if (cpuusage >= 60) {
+        iscpumid = true;
+        iscpuok = false;
+        iscpuisdying = false;
+    } else if (cpuusage < 60) {
+        iscpuok = true;
+        iscpumid = false;
+        iscpuisdying = false;
+    } if (ramusage >= 99) {
+        iscpuisdying= true;
+        iscpumid = false;
+        iscpuok = false;
+    }
+
+    if (iscpuisdying && isramisdying) {
+        status = 1;
+    } else if (iscpumid || isrammid) {
+        status = 2;
+    } else if (isramok || iscpuok) {
+        status = 3;
+    }
+}
+
+async function updatestatus() {
+    if (status === 1) {
         await client.users.profile.set({
             profile: {
                 "status_text": "MY PC IS GOING TO EXPLODE",
@@ -34,9 +80,8 @@ async function updatestatus() {
                 "status_expiration": 0
             }
         });
-        status = 1;
     }
-    else if(ramusage >= 95 && cpuusage >= 80) {
+    else if(status === 2) {
         await client.users.profile.set({
             profile: {
                 "status_text": "Yeah my pc usage is at mid but it think its okay",
@@ -44,9 +89,7 @@ async function updatestatus() {
                 "status_expiration": 0
             }
         });
-        status = 2;
-    }
-    else {
+    } else if(status === 3){
         await client.users.profile.set({
             profile: {
                 "status_text": "okie my pc usage is fine",
@@ -54,7 +97,6 @@ async function updatestatus() {
                 "status_expiration": 0
             }
         });
-        status = 3;
     }
 }
 
@@ -63,6 +105,7 @@ console.log("selfbot started!!!");
 
 setInterval(async () => {
     try {
+        getstatusvalue();
         if (laststatus === status) return;
         await updatestatus();
         laststatus = status;
